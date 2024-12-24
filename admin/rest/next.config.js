2f4825e222887f2a9079e6d3b0c437443d4fa53c
AlexanderSafiulin <115018603+Ganjamill@users.noreply.github.com>
2024-12-24 17:26:14 +0700

    d
    d

--- admin/rest/next.config.js
+++ admin/rest/next.config.js
@@ -14,6 +14,8 @@ const nextConfig = {
   i18n,
   images: {
     domains: [
+      'ganjamill.io',
+      'admin.ganjamill.io',
       'via.placeholder.com',
       'res.cloudinary.com',
       's3.amazonaws.com',
